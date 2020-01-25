fca6adc70c26d846f58400e3f455aa4a5fadf14c
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-01-25 11:58:25 +0100

    We can play against the engine.

--- lib/game.dart
+++ lib/game.dart
@@ -9,6 +9,7 @@ import 'dart:async';
 class GamePage extends StatelessWidget {
   static const platform =
       const MethodChannel('loloof64.chess_utils/engine_discovery');
+  ChessBoard _chessBoard;
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +17,26 @@ class GamePage extends StatelessWidget {
 
     final screenDimensions = MediaQuery.of(context).size;
 
+    final startFen = '4k3/8/8/3KP3/8/8/8/8 w - - 0 1';
+    final whiteToPlayFirst = startFen.split(' ')[1] == 'w';
+    final whiteType = whiteToPlayFirst ? PlayerType.Human : PlayerType.Computer;
+    final blackType = whiteToPlayFirst ? PlayerType.Computer : PlayerType.Human;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Chess exercises manager'),
       ),
       body: Center(
         child: new OrientationBuilder(builder: (context, orientation) {
-          return ChessBoard(
+          _chessBoard = ChessBoard(
             orientation == Orientation.portrait ? screenDimensions.width - 20.0 : screenDimensions.height - 90.0,
             gameEndedHandler: (type) => handleGameEnded(context, type),
+            startFen: startFen,
+            whitePlayerType: whiteType,
+            blackPlayerType: blackType,
+            engineTurnCallback: _engineTurnHandler,
           );
+          return _chessBoard;
         }),
       ),
     );
@@ -52,18 +63,27 @@ class GamePage extends StatelessWidget {
       case EndType.FoldRepetitionsDraw:
         message = "Draw by 3-folds repetitions";
         break;
+      default:
+        return;
     }
     final snackBar = SnackBar(content: Text(message));
     Scaffold.of(context).showSnackBar(snackBar);
   }
 
-  Future<void> _getEngineOutput() async {
+  void _engineTurnHandler(String currentPosition) {
+    _getEngineOutputAndProcessIt(currentPosition)
+        .then((value) {})
+        .catchError((err) {
+          print("Failed to run engine turn handler: $err");
+    });
+  }
+
+  Future<void> _getEngineOutputAndProcessIt(String currentPosition) async {
     try {
-      var positionFen = "8/6k1/8/6p1/6P1/4K2P/8/8 w - - 0 1";
       await platform.invokeMethod("sendCommandToEngine", 'ucinewgame');
       await platform.invokeMethod(
-          "sendCommandToEngine", "position fen $positionFen");
-      await platform.invokeMethod("sendCommandToEngine", "go depth 14");
+          "sendCommandToEngine", "position fen $currentPosition");
+      await platform.invokeMethod("sendCommandToEngine", "go depth 20");
     } catch (e) {
       print("Failed to get engine output: $e");
     }
@@ -71,7 +91,25 @@ class GamePage extends StatelessWidget {
 
   Future<void> _processEngineOutput(MethodCall call) async {
     if (call.method != 'processEngineOutput') return;
-    var line = call.arguments;
-    print(line);
+    String line = call.arguments;
+    if ( ! line.startsWith('bestmove') ) return;
+
+    final moveStr = line.split(' ')[1];
+    final CellCoordinates start = _coordinatesStringToCellCoordinates(moveStr.substring(0, 2));
+    final CellCoordinates end = _coordinatesStringToCellCoordinates(moveStr.substring(2, 4));
+    final MoveCoordinates move = MoveCoordinates(start: start, end: end);
+    final String promotion = moveStr.length >= 5 ? moveStr[4] : null;
+
+    _chessBoard.tryToCommitComputerMove(move, promotion);
+  }
+
+  CellCoordinates _coordinatesStringToCellCoordinates(String coordinates) {
+    final asciiCodeA = 97;
+    final asciiCode1 = 49;
+
+    final file = coordinates.codeUnitAt(0) - asciiCodeA;
+    final rank = coordinates.codeUnitAt(1) - asciiCode1;
+
+    return CellCoordinates(file: file, rank: rank);
   }
 }
